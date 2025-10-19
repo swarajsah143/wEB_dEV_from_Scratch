9d27b736ee1e98f6be2c734208b54b0d41f6141b
Swaraj Sah <swarajsah143@gmail.com>
2025-10-19 19:58:54 +0530

    checked

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-console.log("Hi I am Js file");
+
+const prompt = require("prompt-sync")();
+let name1=prompt("Enter Boy name: ");
+let name2=prompt("Enter Girl name: ");
+
+let rand= Math.random();
+var num=rand*100;
+let dice=Math.floor(num)+1;
+
+console.log("Your love percentage is "+dice);
