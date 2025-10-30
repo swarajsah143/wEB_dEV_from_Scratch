a8e2922d73f1784fdb837eebb5b3be2ae7802dcf
Swaraj Sah <swarajsah143@gmail.com>
2025-10-30 18:12:45 +0530

    js concept from yt

---
+++ js-shradha/index.js
@@ -0,0 +1,15 @@
+// marks=[85,98,66,78,88,45]
+// let sum=0
+// for(let idx=0;idx<marks.length;idx++){
+//     sum=sum+marks[idx]
+// }
+// console.log(sum/marks.length)
+
+
+prices=[120,223,444,243,234,765]
+for (let index = 0; index < prices.length; index++) {
+    prices[index]=0.9*prices[index]
+    console.log(prices[index])
+    
+}
+console.log(prices)
